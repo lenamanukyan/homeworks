e44cf1a975e6238925b5793ea3c7dceec1c49895
lenamanukyan <101973403+lenamanukyan@users.noreply.github.com>
2022-06-17 06:44:14 -0700

    For each done (#1)
    * forEach init
    
    * forEach done
    
    * resolved comments

---
+++ forEach.js
@@ -0,0 +1,13 @@
+const family = ["Sofi","Hen", "Len"];
+
+function customForEach(arr, callback){
+  for(let i = 0; i < arr.length; i++){
+    callback(arr[i], i, arr);
+  }
+};
+
+function cb(item, index, arr){
+  console.log(item, index, arr)
+};
+
+customForEch(family, cb);
