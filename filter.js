5e3c13d47d28ac95e52cbc9a1749ed03e0f90c3e
lenamanukyan <lena.manukyan55@gmail.com>
2022-07-11 17:01:45 +0400

    filter done

--- filter.js
+++ filter.js
@@ -26,7 +26,7 @@ console.log(sortArr);
 // Sort 2 homework
 const sortArr2 = ["HTML", "JavaScript", "CSS"]; 
 function copySorted(arr){ 
-  cognst newArrSort = arr.concat()
+  const newArrSort = arr.concat()
    return newArrSort.sort() 
 }; 
 const sorted = copySorted(sortArr2); 
--- forEach.js
+++ forEach.js
@@ -11,25 +11,3 @@ function cb(item, index, arr){
 };
 
 customForEach(family, cb);
-// Slice
-const array = ["t", "e", "s", "t"];
-
-function customSlice(start, end) {
-  const newArr = [];
-  for(let i = start; i < array.length; i++){
-    newArr.push(array[i]);
-    if (array[i] === array[end - 1]) return newArr ;
-  }
-};
-
-// IndexOf
-function customIndexOf(item){
-  for (let i = 0; i < array.length; i++){
-    if(array[i] === item){
-      return i
-    }
-  }
-};
-
-console.log(customSlice(0,3));
-console.log(customIndexOf("e"));
