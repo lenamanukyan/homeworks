5f367593f030baac4c2882072ee7a7a4a620fcf4
Lena <101973403+lenamanukyan@users.noreply.github.com>
2022-08-25 11:30:42 +0400

    Homework (#3)
    * forEach init
    
    * forEach done
    
    * resolved comments
    
    * homework filter
    
    * homeworks
    
    * homeworks
    
    * homeworks
    
    * homework done
    
    * filter done
    
    * slice,indexof and concat
    
    * bruh
    
    * resolved comments
    
    * fixed bugs
    
    * done

--- forEach.js
+++ forEach.js
@@ -11,3 +11,46 @@ function cb(item, index, arr){
 };
 
 customForEach(family, cb);
+
+// Slice
+const array = ["t", "e", "s", "t"];
+
+function slice(arr,start,end) {
+  const finish = end || arr.length;
+  const newArr = [];
+  for(let i = start; i < finish; i++){
+    newArr.push(arr[i]);
+  }
+  return newArr;
+};
+ 
+console.log('Slice', slice(array, 0));
+
+// IndexOf
+function indexOf(item) {
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] === item) {
+      return i;
+    }
+  }
+  return -1;
+};
+console.log(indexOf("s"));
+
+//Concat
+
+function concat(...arrays) {
+  const arr = [];
+  for(let i = 0; i < arrays.length; i++) {
+    const isElementArray  = Array.isArray(arrays[i]);
+    if(isElementArray){
+      for(let j = 0; j < arrays[i].length; j++){
+        arr.push(arrays[i][j]);
+      }
+    } else {
+      arr.push(arrays[i]);
+    }
+  }
+  return arr;
+}
+console.log("Concat", concat([1, 2, 3, 4], ["d", 58, 5], [99], "k"));
