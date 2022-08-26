5ded0a4d7b8bf7e0e44a99f0d9fca701162af531
Lena <101973403+lenamanukyan@users.noreply.github.com>
2022-08-26 20:59:42 +0400

    Slice function (#4)

--- forEach.js
+++ forEach.js
@@ -12,19 +12,6 @@ function cb(item, index, arr){
 
 customForEach(family, cb);
 
-// Slice
-const array = ["t", "e", "s", "t"];
-
-function slice(arr,start,end) {
-  const finish = end || arr.length;
-  const newArr = [];
-  for(let i = start; i < finish; i++){
-    newArr.push(arr[i]);
-  }
-  return newArr;
-};
- 
-console.log('Slice', slice(array, 0));
 
 // IndexOf
 function indexOf(item) {
