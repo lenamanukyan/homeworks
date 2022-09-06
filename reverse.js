3adf0c339da496244c3f8b8a1cfd6bdb44237f07
leanmanukyan <lena.manukyan55@gmail.com>
2022-09-06 16:31:26 +0400

    new addition

--- reverse.js
+++ reverse.js
@@ -1,11 +1,12 @@
-let arr = [1, 2, 3, 6, 4];
+let arr = [1, 2, 3, 6, 4,];
 
-function reverse (arr){
-  let newArr = [];
-  for (let i = arr.length - 1; i >= 0; i--){
-    newArr.push(arr[i]);
+function reverse(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    arr.push(arr[i]);
+  };
+  for (let j = 0; j < arr.length / 1; j++) {
+    arr.shift()
   }
-  arr = newArr
   return arr;
 };
 
